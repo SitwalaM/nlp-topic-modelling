c2d491ca3f83cb0a7a3da837caf5235b207b8de0
SitwalaM <47774622+SitwalaM@users.noreply.github.com>
2022-04-06 18:46:54 +0200

    add sql write file

---
+++ scripts/write_table.py
@@ -0,0 +1,52 @@
+import pandas as pd
+import sqlalchemy
+from sqlalchemy import create_engine
+
+
+
+#credentials
+user = 'root'
+passw = 'j'  #insert your password here
+host =  'localhost'
+port = 3306
+database = 'nlp'
+
+database_connection = create_engine('mysql+mysqlconnector://{0}:{1}@{2}/{3}'.
+                                               format(user, passw, 
+                                                      host, database), pool_recycle=1, pool_timeout=57600).connect()
+
+
+
+def update_db_with_data(database_connection, dataframe, table_name, dtypes_dictionary):
+    ''' function used to connect to update tables in the sql database
+    inputs
+    ------
+    database_connection : connection object using sqlalchemy "create engine"
+    dataframe: pandas datarame to be added to the database
+    table_name: string name of table in database
+    dtypes_dictionary: dictionary of datatypes for columns in the dataframe
+    
+    outputs
+    -------
+    None
+    '''
+
+    dataframe.to_sql(con=database_connection, 
+                     name=table_name, 
+                     if_exists='replace',
+                     dtype = dtypes_dictionary,
+                     chunksize=1000)
+    return None
+
+
+types_dictionary = {"id": sqlalchemy.types.String ,
+                   "retweet_count": sqlalchemy.types.Numeric,
+                    "date_created": sqlalchemy.types.DateTime(),
+                    "tweet": sqlalchemy.types.String,
+                    "clean_tweet": sqlalchemy.types.Numeric,
+                    "topic": sqlalchemy.types.Numeric       
+                    }
+
+dataframe = pd.read_csv("output.csv")
+
+update_db_with_data(database_connection, dataframe, "topics", dtypes_dictionary):
